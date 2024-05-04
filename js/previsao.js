0b37efbb571730810dabba22a916d6e99dafcdd8
01101-kairo <kairomilhomem@gmail.com>
2024-05-04 16:50:33 -0300

    removendo o for do js previsao

--- js/previsao.js
+++ js/previsao.js
@@ -5,18 +5,16 @@ let temperatura = "num"
 console.log(horaFormatada)
 
 async function getPrevisao(){
-  const latitude = document.querySelector('input#latitude').value
-  const longitude = document.querySelector('input#longitude').value
+  const latitude = document.querySelector('#latitude').value
+  const longitude = document.querySelector('#longitude').value
   try{
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`)
-    const data = await response.json()
-    console.log(data)
-    document.getElementById('previsao').innerHTML = ""
-    for (let index = 0; index <= horaAtual; index++) {
-      temperatura = data.hourly.temperature_2m[index]
-    }
-      document.getElementById('previsao').innerHTML = `<span> ${temperatura}° C</span>`
-  } catch (error){
-    alert(error.message)
+    const data = await response.json();
+
+    const temperatura = data.current.temperature_2m;
+    console.log('temperatura:', temperatura)
+    document.getElementById('previsao').innerHTML = `<span>${temperatura}° C</span>`;
+  } catch (error) {
+    alert(error.message);
   }
 }
