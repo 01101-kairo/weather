799e96706195eaaa68476e177e30a1984a6d0c25
01101-kairo <kairomilhomem@gmail.com>
2024-05-20 14:15:46 -0300

    add: usaei grid na tabela de cep e coloquei uma condição no js do cep

--- js/cep.js
+++ js/cep.js
@@ -7,9 +7,10 @@ async function getApiCep(){
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     const data = await response.json()
     console.log(data)
-    document.getElementById('logradouro').innerHTML = `<p> ${data.logradouro} </p>`
-    document.getElementById('bairro').innerHTML = `<p> ${data.bairro} </p>`
-    document.getElementById('uf').innerHTML = `<p> ${data.localidade} </p>`
+    input = `<input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" />`
+    data.logradouro? document.getElementById('logradouro').innerHTML = `<p> ${data.logradouro} </p>` : document.getElementById('logradouro').innerHTML = input
+    data.bairro? document.getElementById('bairro').innerHTML = `<p> ${data.bairro} </p>` : document.getElementById('bairro').innerHTML = input
+    data.localidade? document.getElementById('uf').innerHTML = `<p> ${data.localidade} </p>` : document.getElementById('uf').innerHTML = input
   } catch(error){
     alert(error.message)
   }
